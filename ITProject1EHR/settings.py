1db4e49055c80439210c65107bd504922d427d1d
Ptennisnet <148290329+Ptennisnet@users.noreply.github.com>
2024-06-05 13:57:51 +1000

    CryptoHeaderIntegration

--- ITProject1EHR/settings.py
+++ ITProject1EHR/settings.py
@@ -47,7 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'patients.middleware.IHLHeaderMiddleware',
+    'patients.middleware.IHLInfoHeaderMiddleware',
+    'patients.middleware.IHLCertificateHeaderMiddleware',
 ]
 
 ROOT_URLCONF = 'ITProject1EHR.urls'
--- patients/middleware.py
+++ patients/middleware.py
@@ -1,11 +1,30 @@
-class IHLHeaderMiddleware:
+import os
+
+
+class IHLInfoHeaderMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
 
-        response['X-IHLProtection-Header'] = ('This digital infrastructure is protected under humanitarian law. '
-                                              'Any unauthorised interference with this digital system may be '
-                                              'prosecutable under Geneva Conventions IV Article 18.')
+        response['X-IHL-Protection-Header'] = ('This digital infrastructure is protected under humanitarian law. '
+                                               'Any unauthorised interference with this digital system may be '
+                                               'prosecutable under Geneva Conventions IV Article 18.')
+        return response
+
+
+class IHLCertificateHeaderMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        directory = os.path.dirname(__file__)
+        certificate_path = os.path.join(directory[:-8], 'IHLCertificate/certificate.pem')
+        with open(certificate_path) as cert_file:
+            certificate = cert_file.read()
+
+        response['X-IHL-Certificate-Header'] = certificate
         return response
